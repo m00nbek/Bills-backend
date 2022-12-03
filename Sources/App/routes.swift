d2d226230f688c1945b99cfa657988548176be19
m00nbek <melikulov990@gmail.com>
2022-12-03 17:15:52 +0500

    creating feed item on POST request on `feed`

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,14 @@ func routes(_ app: Application) throws {
     app.get("feed") { req async throws in
         try await Expense.query(on: req.db).all()
     }
+    
+    app.post("feed") { req -> EventLoopFuture<Expense> in
+        let expense = try req.content.decode(Expense.self)
+        
+        // setting the timestamp
+        expense.timestamp = Date().ISO8601Format()
+        
+        return expense.create(on: req.db)
+            .map { expense }
+    }
 }
