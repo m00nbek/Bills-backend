dda8eea7268ac5f808e666078f74bd0c98432b99
m00nbek <melikulov990@gmail.com>
2022-12-03 17:17:45 +0500

    added some comments

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,21 +1,24 @@
 import Vapor
 
 func routes(_ app: Application) throws {
+    
+    // home page `/`
     app.get { req async in
         "Ohayo!"
     }
     
+    // query all the feed items
     app.get("feed") { req async throws in
         try await Expense.query(on: req.db).all()
     }
-    
+   
+    // create new feed item
     app.post("feed") { req -> EventLoopFuture<Expense> in
         let expense = try req.content.decode(Expense.self)
         
         // setting the timestamp
         expense.timestamp = Date().ISO8601Format()
         
-        return expense.create(on: req.db)
-            .map { expense }
+        return expense.create(on: req.db).map { expense }
     }
 }
