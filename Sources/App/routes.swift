2bb6d32aeb1429b7957009ee83acbdfff185a851
m00nbek <melikulov990@gmail.com>
2023-01-24 15:06:03 +0500

    update routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,13 +8,13 @@ func routes(_ app: Application) throws {
     }
     
     // query all the feed items
-    app.get("bills", "test-feed") { req async throws in
+    app.get("v1", "feed") { req async throws in
         let items = try await Expense.query(on: req.db).all()
         return ItemWrapper(items: items)
     }
    
     // create new feed item
-    app.post("bills", "test-feed") { req -> EventLoopFuture<Expense> in
+    app.post("v1", "feed") { req -> EventLoopFuture<Expense> in
         let expense = try req.content.decode(Expense.self)
         
         // setting the timestamp
